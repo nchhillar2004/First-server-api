e3008392c62bb3f91fc06c425ff5c19f3fae686e
nchhillar2004 <nchhillar2004@gmail.com>
2023-09-18 23:04:51 +0530

    added server.config.json

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 const connectToMongo  = require('./db');
 const express = require('express');
-
+const config = require('./server.config.json');
 connectToMongo();
 
 const app = express()
-const port = 3000 // Port to run localhost
+const port = config.app.port; // Port to run localhost
+const name = config.app.name;
 
 app.use(express.json());  // Parse incoming JSON data from HTTP requests
 
@@ -13,5 +14,5 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/notes', require('./routes/notes'));
 
 app.listen(port, () => {
-  console.log(`Example app listening at : http://localhost:${port}`)
+  console.log(`${name} app server listening at : http://localhost:${port}`)
 })
