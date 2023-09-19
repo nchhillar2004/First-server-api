b97323ec0f5ef9685725a8204e566ae7905a0ed7
nchhillar2004 <nchhillar2004@gmail.com>
2023-09-19 12:44:55 +0530

    commit 3

--- server/routes/notes.js
+++ server/routes/notes.js
@@ -57,4 +57,48 @@ router.post(
     }
 );
 
+// ROUTE 3 : Update an existing Note using : PUT "/api/notes/updatenote"
+router.put(
+    "/updatenote/:id",
+    fetchUser,
+    // Validating notes details using express-validator
+    [
+        body("title", "Title must be atleast 3 characters.").isLength({
+            min: 3,
+        }),
+        body(
+            "description",
+            "Description must be atleast 5 characters."
+        ).isLength({ min: 5 }),
+    ],
+    async (req, res) => {
+        try {
+            const {title, description, tag} = req.body;
+
+            // Create a newNote object
+            const newNote = {};
+            if(title){newNote.title = title};
+            if(description){newNote.description = description};
+            if(tag){newNote.tag = tag};
+
+            // Find the note to be updated and update it
+            let note = await Notes.findById(req.params.id);
+            if(!note){res.status(404).send("Not Found")}
+
+            if(note.user.toString() !== req.user.id){
+                return res.status(401).send("Not allowed");
+            }
+
+            note = await Notes.findByIdAndUpdate(req.params.id, {$set: newNote}, {new:true});
+            res.json({note});
+
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).send(
+                "Internal Server Error : unable to update note"
+            );
+        }
+    }
+    );
+
 module.exports = router;
