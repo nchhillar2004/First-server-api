c297707c7aa9b255c8275ff6b584c702b771f3a3
Nishant Chhillar <115921678+nchhillar2004@users.noreply.github.com>
2023-09-20 08:10:09 +0530

    Update server.config.js

--- server/server.config.js
+++ server/server.config.js
@@ -2,13 +2,15 @@ const config = {
     app : {
         name : "App Name",
         port : "5000",
-    }
+    },
     database : {
         name : "Database Name to connect with",
         port : "Data base port ex- 27017",
+        username : "Your MongoDB username",
+        password : "Your MongoDB password",
     }
 }
 
-const config.database.uri = "mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/[defaultauthdb][?options]]"
+const config.database.uri = `mongodb+srv://${config.database.username}:${config.database.password}@cluster0.rzhlemb.mongodb.net/${config.database.name}?retryWrites=true&w=majority`;
 
 module.exports = config;
